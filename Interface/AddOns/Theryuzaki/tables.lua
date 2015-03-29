d4472b682311c8892099e0b56d497b868d8a4b6d
shikulja <shikulja@rambler.ru>
2015-03-29 09:41:48 +0300

    black list interrupts
    черный список для прерываний

---
+++ Interface/AddOns/Theryuzaki/tables.lua
@@ -0,0 +1,12 @@
+interruptBlacklist = {
+	["High Priestess Kilnara"] = {[1] = "Shadow Bolt"},
+	["Venomancer Mauri"] = {[1] = "Poison Bolt"},
+	["Hex Lord Malacrass"] = {[1] = "Chain Lightning",[2] = "Mind Blast"},
+	["Maloriak"] = {[1] = "Release Aberrations"},
+	["Naz'jar Spiritmender"] = {[1] = "Wrath"},
+	["Stonecore Rift Conjurer"] = {[1] = "Shadow Bolt"},
+	["Temple Adept"] = {[1] = "Holy Smite"},
+	["Twilight Portal Shaper"] = {[1] = "Shadow Bolt"},
+	["Lesser Priest of Bethekk"] = {[1] = "Shadow Bolt"},
+	["Stonecore Earthshaper"] = {[1] = "Lava Burst",[2] = "Ground Shock"}
+};
